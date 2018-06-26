d5b45c40f9212f1efbf586970ca6c51532a8025a
matthewrungwe <40231775+matthewrungwe@users.noreply.github.com>
2018-06-26 16:53:49 +0200

    PAP
    Pixel art files

---
+++ designs.js
@@ -0,0 +1,26 @@
+//Selecting the document;
+$(document).ready(function(){
+    $('#sizePicker').submit(function makeGrid(grid){
+        $('table tr').remove();
+        var rows = $('#inputHeight').val();
+        var col = $('#inputWidth').val();
+        //Adding elements to the html document.
+        for(var r = 1; r <= rows; r++){
+            $('table').append('<tr> </tr>');
+            for(var c = 1; c <= col; c++){
+                $('tr:last').append('<td> </td>');
+                $('td').attr("class", 'cells');
+            }
+        }
+        //Prevent any default responses by the grid
+        grid.preventDefault();
+
+        //Selectors and (shorthand)listeners
+        $('.cells').click(function(event){
+            var paint = $('#colorPicker').val();
+            $(event.target).css('background-color', paint);
+        });
+    });
+});
+
+//Code by MattJR(Matthew Jeremiah Rungwe) </Code>
