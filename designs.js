2d80b4c9ec1054b2ba00c01df2fddc248a64e9aa
MattJR <40231775+matthewrungwe@users.noreply.github.com>
2018-07-01 21:33:33 +0200

    Update designs.js
    Changed variable declarations

--- designs.js
+++ designs.js
@@ -2,23 +2,26 @@
 $(document).ready(function(){
     $('#sizePicker').submit(function makeGrid(grid){
         $('table tr').remove();
-        var rows = $('#inputHeight').val();
-        var col = $('#inputWidth').val();
+        const rows = $('#inputHeight').val();
+        const col = $('#inputWidth').val();
+
         //Adding elements to the html document.
-        for(var r = 1; r <= rows; r++){
+        for(let r = 1; r <= rows; r++){
             $('table').append('<tr> </tr>');
-            for(var c = 1; c <= col; c++){
+            for(let c = 1; c <= col; c++){
                 $('tr:last').append('<td> </td>');
                 $('td').attr("class", 'cells');
             }
         }
+
         //Prevent any default responses by the grid
         grid.preventDefault();
 
         //Selectors and (shorthand)listeners
-        $('.cells').click(function(event){
-            var cellColour = $('#colorPicker').val();
+        $('.cells').click(function(event){            
+            const cellColour = $('#colorPicker').val();
             $(event.target).css('background-color', cellColour);
+
         });
     });
 });
